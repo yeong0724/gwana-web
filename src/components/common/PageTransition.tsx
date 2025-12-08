8717e56f48ab46c38f79325ef2cc1dbcfad0248e
FILO (Jinyeong-Kim) <76084514+yeong0724@users.noreply.github.com>
2025-12-08 13:25:20 +0900

    PageTransition.tsx 업데이트

--- src/components/common/PageTransition.tsx
+++ src/components/common/PageTransition.tsx
@@ -34,7 +34,7 @@ function PageTransition({ children }: PageTransitionProps) {
   }
 
   return (
-    <AnimatePresence mode="sync">
+    <AnimatePresence mode="wait" initial={false}>
       <motion.div
         key={pathname}
         variants={variants}
