83857c50414dac3c4d4e20b10c269220ac64fa99
Jinyeong Kim <kid4211@gmail.com>
2026-02-27 00:14:15 +0900

    로그인 응답 딜레이 추가

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,7 +1,9 @@
 import { postAxios } from '@/lib/api';
+import { delayAsync } from '@/lib/utils';
 import type { ApiResponse, LoginResponse } from '@/types';
 
 const getAccessTokenByKakaoCode = async <T>(params: T) => {
+  await delayAsync(1000);
   return postAxios<ApiResponse<LoginResponse>>({
     url: '/user/callback',
     params,
@@ -29,4 +31,4 @@ const kakaoLogout = async <T>(params: T) => {
   });
 };
 
-export { fetchLogin, getAccessTokenByKakaoCode, kakaoLogout, refreshAccessToken };
+export { getAccessTokenByKakaoCode, fetchLogin, refreshAccessToken, kakaoLogout };
